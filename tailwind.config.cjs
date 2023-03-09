628854be96238941ec0fff58f1cc7d82a3ff78a3
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-09 16:33:58 +0530

    initial commit.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  mode: 'jit',
+  darkMode: "media",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
