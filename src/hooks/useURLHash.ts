cfe26392e3040e225696c22042d60545c443df37
vinod liyanage <vinodsliyanage@gmail.com>
2023-03-09 16:41:55 +0530

    create useURLHash custom hook.

---
+++ src/hooks/useURLHash.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { createHash } from "../utils/utils";
+
+
+export function useURLHash() {
+  const [hash, setHash] = useState('');
+
+  useEffect(() => {
+    const handleTab = (tabs: chrome.tabs.Tab[]) => {
+      const _url = tabs?.[0]?.url;
+      if (!_url) return;
+
+      const url = new URL(_url);
+      const hash = createHash(url.origin + url.pathname);
+      setHash(String(hash));
+    };
+    chrome.tabs.query({ active: true, currentWindow: true }, handleTab);
+  }, []);
+
+  return hash;
+}
