d8ae941c49280bf951c6f71128e73e8bc7229901
vinod liyanage <vinodsliyanage@gmail.com>
2023-04-24 15:59:03 +0530

    create validationMessages.

---
+++ src/data/validationMessages.ts
@@ -0,0 +1,35 @@
+export const validationMessages = {
+    EMAIL_NOT_VALID: 'Email is not valid',
+    EMAIL_REQUIRED: 'Email is required',
+
+    CURRENT_PASSWORD_MIN_LENGTH: 'Current password must be at least 8 characters',
+    CURRENT_PASSWORD_MAX_LENGTH: 'Current password must be at most 255 characters',
+    CURRENT_PASSWORD_REQUIRED: 'Current password is required',
+
+    NEW_PASSWORD_MIN_LENGTH: 'New password must be at least 8 characters',
+    NEW_PASSWORD_MAX_LENGTH: 'New password must be at most 255 characters',
+    NEW_PASSWORD_REQUIRED: 'New password is required',
+
+    PASSWORD_NOT_VALID: 'Password is not valid',
+    PASSWORD_NOT_MATCH: 'Password does not match',
+    PASSWORD_MIN_LENGTH: 'Password must be at least 8 characters',
+    PASSWORD_MAX_LENGTH: 'Password must be at most 255 characters',
+    PASSWORD_REQUIRED: 'Password is required',
+
+    CONFIRM_PASSWORD_REQUIRED: 'Confirm password is required',
+
+    FIRST_NAME_MIN_LENGTH: 'First name must be at least 2 characters',
+    FIRST_NAME_MAX_LENGTH: 'First name must be at most 255 characters',
+    FIRST_NAME_REQUIRED: 'First name is required',
+
+    LAST_NAME_MIN_LENGTH: 'Last name must be at least 2 characters',
+    LAST_NAME_MAX_LENGTH: 'Last name must be at most 255 characters',
+    LAST_NAME_REQUIRED: 'Last name is required',
+
+    USERNAME_MIN_LENGTH: 'Username must be at least 2 characters',
+    USERNAME_MAX_LENGTH: 'Username must be at most 255 characters',
+    USERNAME_REQUIRED: 'Username is required',
+
+    AGREE_TERMS: 'You must agree to terms and conditions',
+    AGREE_TERMS_REQUIRED: 'Agree terms and conditions is required',
+}
