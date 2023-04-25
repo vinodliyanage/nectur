18e2163a2a2361fc77b5b4b0b7d533124cffd44e
vinod liyanage <vinodsliyanage@gmail.com>
2023-04-25 16:35:43 +0530

    optimize the code.

--- src/routes/EmailConfirmation.tsx
+++ src/routes/EmailConfirmation.tsx
@@ -3,6 +3,19 @@ import email from "../assets/images/email.png";
 import Button from "../components/Button";
 import Navbar from "../components/Navbar";
 import { useAuth } from "../contexts/AuthContext";
+import { classNames } from "../utils/utils";
+
+const initialStyles = {
+  h1: "text-2xl mb-2",
+  h2: "text-base",
+  fontLight: "font-light",
+  fontBold: "font-bold",
+  base: "leading-tight tracking-tight text-gray-900 dark:text-white",
+};
+
+const h1Styles = classNames(initialStyles, ["h1", "font-bold", "base"]);
+const h2LightStyles = classNames(initialStyles, ["h2", "fontLight", "base"]);
+const h2BoldStyles = classNames(initialStyles, ["h2", "fontBold", "base"]);
 
 const EmailConfirmation: React.FC = () => {
   const [isDisabled, setIsDisabled] = useState(false);
@@ -23,26 +36,21 @@ const EmailConfirmation: React.FC = () => {
   const handleResend = () => {
     setIsDisabled(true);
     setCountdown(30);
+    // TODO: send email verification
   };
 
   return (
     <>
-      <div className="w-11/12 flex flex-col items-center justify-center px-6 py-8 mx-auto">
+      <div className="px-6 py-8 mx-auto">
         <Navbar backTo="/Signin" />
 
-        <div className="w-full p-6 flex flex-col gap-5 bg-light-300 rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full p-6 flex flex-col gap-5 rounded-lg shadow bg-light-300 dark:border dark:bg-gray-800 dark:border-gray-700">
           <img className="w-24 h-24 m-auto" src={chrome.runtime.getURL(email)} alt="" />
           <div className="flex flex-col items-center space-y-2 text-center">
-            <h1 className="text-2xl font-bold mb-2 leading-tight tracking-tight text-gray-900 dark:text-white">
-              Confirm your email address
-            </h1>
-            <h2 className="text-base font-light leading-tight tracking-tight text-gray-900 dark:text-white">
-              We sent a verification email to:
-            </h2>
-            <h2 className="text-base font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
-              {userEmail}
-            </h2>
-            <h2 className=" text-base font-light leading-tight tracking-tight text-gray-900 dark:text-white">
+            <h1 className={h1Styles}>Confirm your email address</h1>
+            <h2 className={h2LightStyles}>We sent a verification email to:</h2>
+            <h2 className={h2BoldStyles}>{userEmail}</h2>
+            <h2 className={h2LightStyles}>
               Check your email and click on the confirmation link to continue.
             </h2>
           </div>
